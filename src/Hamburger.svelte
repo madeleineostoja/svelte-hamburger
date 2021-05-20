cc541e2014a7edeb37270520582357e943db9468
madeleineostoja <madi@madeleineostoja.com>
2021-05-20 22:49:37 +1200

    fix event propagation

--- src/Hamburger.svelte
+++ src/Hamburger.svelte
@@ -7,7 +7,43 @@
   export let width = 32;
   export let height = 24;
   export let stroke = 2;
+
 </script>
+
+<div class:open on:click>
+  <svg style="min-height: {height}px;" {width} {height}>
+    <line
+      id="top"
+      style="stroke-width: {stroke}; {open
+        ? `transform: translate(${height / 3}px, 0px) rotate(45deg);`
+        : ''}"
+      x1="0"
+      y1="2"
+      x2={width}
+      y2="2"
+    />
+    <line
+      id="middle"
+      style="stroke-width: {stroke}"
+      x1="0"
+      y1={height / 3 + stroke}
+      x2={width}
+      y2={height / 3 + stroke}
+    />
+    <line
+      id="bottom"
+      style="stroke-width: {stroke}; {open
+        ? `transform: translate(-${width / 4}px, ${
+            width / 3
+          }px) rotate(-45deg);`
+        : ''}"
+      x1="0"
+      y1={height / 3 + height / 3 + stroke}
+      x2={width}
+      y2={height / 3 + height / 3 + stroke}
+    />
+  </svg>
+</div>
 
 <style>
   svg {
@@ -30,33 +66,5 @@
   .open #bottom {
     transform: translate(-12px, 9px) rotate(-45deg);
   }
-</style>
 
-<div class:open on:click={() => (open = !open)}>
-  <svg style="min-height: {height}px;" {width} {height}>
-    <line
-      id="top"
-      style="stroke-width: {stroke}; {open ? `transform: translate(${height / 3}px, 0px) rotate(45deg);` : ''}"
-      x1="0"
-      y1="2"
-      x2={width}
-      y2="2"
-    />
-    <line
-      id="middle"
-      style="stroke-width: {stroke}"
-      x1="0"
-      y1={height / 3 + stroke}
-      x2={width}
-      y2={height / 3 + stroke}
-    />
-    <line
-      id="bottom"
-      style="stroke-width: {stroke}; {open ? `transform: translate(-${width / 4}px, ${width / 3}px) rotate(-45deg);` : ''}"
-      x1="0"
-      y1={height / 3 + height / 3 + stroke}
-      x2={width}
-      y2={height / 3 + height / 3 + stroke}
-    />
-  </svg>
-</div>
+</style>
